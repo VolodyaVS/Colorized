c53c878266e09760e951c606c84f14555fc04d16
Volodya <volodyavolga@protonmail.ch>
2020-10-19 22:10:45 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Vladimir Stepanchikov on 19.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
